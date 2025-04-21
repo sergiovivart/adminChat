7cd04abf382a91a5a3b7f8420d65742b123e5001
sergiovivart <sevivarte@gmail.com>
2025-04-21 23:21:57 +0200

    cambio sensicllo al sever.js, para que verifique si es el admin el que envio el mensaje

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ let clients = {};
 io.on('connection', (socket) => {
     console.log(`Conectado: ${socket.id}`);
 
+    // con esto registramos los sockets de admin y cliente
     socket.on('register', (role) => {
-        if (role === 'admin') {
+        if (role === 'admin') { // registramos el admin
             adminSocket = socket;
             console.log('Administrador conectado');
-        } else if (role === 'client') {
+        } else if (role === 'client') { // registramos el cliente
             clients[socket.id] = socket;
             console.log('Cliente conectado:', socket.id);
             if (adminSocket) {
@@ -32,8 +33,9 @@ io.on('connection', (socket) => {
         }
     });
 
+    //  Manejamos los mensajes que envien los cleintes
     socket.on('message-from-client', ({ toAdmin, message }) => {
-        if (adminSocket) {
+        if (adminSocket && toAdmin) {
             adminSocket.emit('message-from-client', {
                 clientId: socket.id,
                 message
@@ -41,6 +43,7 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Manejamos los mensajes que envie el admin
     socket.on('message-from-admin', ({ toClientId, message }) => {
         const clientSocket = clients[toClientId];
         if (clientSocket) {
@@ -50,14 +53,15 @@ io.on('connection', (socket) => {
         }
     });
 
+    // en caso de desconcexion
     socket.on('disconnect', () => {
         console.log('Desconectado:', socket.id);
         if (clients[socket.id]) {
-            delete clients[socket.id];
+            delete clients[socket.id];  // quitamos el socket de la lista
             if (adminSocket) {
                 adminSocket.emit('client-disconnected', socket.id);
             }
-        } else if (socket === adminSocket) {
+        } else if (socket === adminSocket) { // quitamos el admin socket
             adminSocket = null;
             console.log('Administrador desconectado');
         }
