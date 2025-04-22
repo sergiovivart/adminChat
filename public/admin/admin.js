78394549e27695b50c2ec42b7d2957050fabce7c
sergiovivart <sevivarte@gmail.com>
2025-04-23 00:54:03 +0200

    mejorado el frontend de la parte del admisnitador y agregado para que se listen los sockets conectados  en una lista en la barra lateral y al desconectarse pues se eleminan de la lista

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,22 +1,33 @@
 const socket = io();
 let currentClientId = null;
 
+// base con modelos para datos
+
+// un modelo de estructura de datos para guardar lso mensajes y el id del cliente
+// en orden con mi mensajes y los mensajes de los clientes
+const messages = [];    
+let clientId = null; // Variable para almacenar el ID del cliente seleccionado
+let clientIdConectado = null; 
+
 socket.emit('register', 'admin');
 
 socket.on('client-connected', (clientId) => {
-    const select = document.getElementById('clientList');
-    const option = document.createElement('option');
-    option.value = clientId;
-    option.text = clientId;
-    select.add(option);
+    const clientListSideBar = document.getElementById('clientListSideBar');
+    const li                = document.createElement('li');
+    li.className            = 'list-group-item d-flex justify-content-between align-items-center';
+    li.innerHTML            = clientId;
+    clientListSideBar.appendChild(li);
     if (!currentClientId) currentClientId = clientId;
 });
 
+
 socket.on('client-disconnected', (clientId) => {
-    const select = document.getElementById('clientList');
-    for (let i = 0; i < select.options.length; i++) {
-        if (select.options[i].value === clientId) {
-            select.remove(i);
+    const clientListSideBar = document.getElementById('clientListSideBar');
+    const items = clientListSideBar.getElementsByTagName('li');
+    
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].textContent === clientId) {
+            clientListSideBar.removeChild(items[i]);
             break;
         }
     }
@@ -28,11 +39,27 @@ socket.on('message-from-client', ({ clientId, message }) => {
     currentClientId = clientId;
 });
 
+
+// selecionamos l id del socket a trabajar
+const ul = document.getElementById('clientListSideBar'); // El contenedor 
+const etiquetaIdConectado = document.getElementById('etiquetaIdConectado');
+
+
+// aqui controlamos a que personas le enviaremo sla
+ul.addEventListener('click', (e) => {
+    if (e.target.classList.contains('list-group-item')) {
+        const selectedClientId = e.target.textContent; // Actualiza el clientId global
+        clientId = selectedClientId; // Actualiza el clientId global
+        etiquetaIdConectado.innerHTML = 'cliente conectado : ' + clientId; // Actualiza el ID del cliente conectado
+    }
+});
+
+// enviamso el mensaje
 function sendMessage() {
     const input = document.getElementById('input');
     const msg = input.value;
     const select = document.getElementById('clientList');
-    const clientId = select.value;
+    // const clientId = select.value;
 
     socket.emit('message-from-admin', {
         toClientId: clientId,
